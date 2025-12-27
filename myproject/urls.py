e9e68c55cdef56b9eb16a2542323d9052fc7c5f0
chukwu prince <chukwuchinemerem258@gmail.com>
2025-12-28 00:15:03 +0100

    options

--- myproject/urls.py
+++ myproject/urls.py
@@ -12,5 +12,3 @@ urlpatterns = [
     path('set-language/', set_language, name='set_language'),
 ]
 
-# Serve media files in development and production (Render)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
